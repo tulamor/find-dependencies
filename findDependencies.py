933bc3c545c26a63f235961edc08ae4b13ee8827
tulamor <romano@linuxmail.org>
2020-03-09 16:41:07 +0100

    globals declared, but no initialized

--- findDependencies.py
+++ findDependencies.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python
 
-from os import environ, popen
+# from os import environ, popen
+import sys, re, os, json, gzip
 from argparse import ArgumentParser
-import sys
-import re
 from glob import glob
+
 
 parser = ArgumentParser()
 parser.add_argument('-rel')
-parser.add_argument('-arch', dest = 'scramarch', default = environ.get("SCRAM_ARCH"))
-parser.add_argument('-scramroot', default = environ.get("SCRAMV1_ROOT"))
+parser.add_argument('-arch', dest = 'scramarch', default = os.environ.get("SCRAM_ARCH"))
+parser.add_argument('-scramroot', default = os.environ.get("SCRAMV1_ROOT"))
 args = parser.parse_args()
 
 rel = args.rel
@@ -17,33 +17,17 @@ scramarch = args.scramarch
 scramroot = args.scramroot
 
 if scramroot is None:
-  scramroot = popen("sh -v scram arch 2>&1 |  grep 'SCRAMV1_ROOT=' | sed 's|;.*||;s|SCRAMV1_ROOT=||;s|\"||g' | sed -e \"s|'||g\"").read()
+  scramroot = os.popen("sh -v scram arch 2>&1 |  grep 'SCRAMV1_ROOT=' | sed 's|;.*||;s|SCRAMV1_ROOT=||;s|\"||g' | sed -e \"s|'||g\"").read()
 
 global name, dir
-name, dir = None, None
-
-
-import os
-cwd = os.getcwd()
-
 uses = {}
 usedby = {}
-beginning = ""
-
-# Traverse desired filesystems
 directory = rel + "/tmp" # root dir for traversing
 
-with open(rel + "/etc/dependencies/uses.out", 'w') as file:
-  for key, value in uses.items():
-    file.write("%s %s\n" % (key,value))
-
-with open(rel + "/etc/dependencies/usedby.out", 'w') as file:
-  for key, value in uses.items():
-    file.write("%s %s\n" % (key,value))
-
-def doexec(*args):
-  print("---- doexec running------" + name)
+def doexec():
+  global uses
   getnext = 0
+  depname = ""
   with open(name, 'r') as file:
     for l in file:
       l = l.rstrip('\n')
@@ -70,46 +54,65 @@ def doexec(*args):
             getnext = 1
         else:
           if re.search( r'^src', sp1[0]):
-            if depname in uses.keys():
-              uses[depname] += tsp1
-            else: uses[depname] = tsp1
-            if tsp1 in usedby.keys():
-              usedby[tsp1] += depname
-            else: usedby[tsp1] = depname
+            if depname not in uses.keys():
+              uses[depname] = "%s " % tsp1
+            else: 
+              uses[depname] += "%s " % tsp1
+            if tsp1 not in usedby.keys():
+              usedby[tsp1] = "%s " % depname
+            else: 
+              usedby[tsp1] += "%s " % depname
 
 def pythonDeps(rel):
   cache = {}
   for root, dirs, files in os.walk("%s/src/" % rel):
     for filename in files:
-      fname = os.path.join(root, filename)
-      print(fname)
-      file = fname
-      file = re.sub(r'^${rel}\/+src\/+', r'', file)
-      if not re.search( r'\/python\/', fname): continue
-      with open(fname, 'r') as f:
-        for line in f.readlines():
-          if 'import' in line:
-            line = line.rstrip ('\n')
-            print(line)
-            if re.search( r'^\s*#', line): continue
-            match =  re.search( r'^\s*from\s+([^\s]+)\s+import\s+', line)
-            if match:
-              for x in import2CMSSWDir(match.group(1), cache):
-                cache["usedby"][x][file] = 1
-                cache["uses"][file][x] = 1
-            elif re.search(r'^\s*import\s+([^\s]+)\s*', line):
-              for x in import2CMSSWDir(match.group(1), cache):
-                cache["usedby"][x][file] = 1
-                cache["uses"][file][x] = 1
-    for type_ in ("uses","usedby"):
-      with open("%s/etc/dependencies/py%s.out" % (rel, type_), 'w') as ref:
-        for x in sorted(cache[type_].keys()):
-          ref.write("%s %s\n" % (x, " ".join(sorted(cache[type_][x].keys()))))
+      fpath = os.path.join(root, filename)
+      if re.search(r'.py$', fpath):
+        fname = fpath
+        file = fname
+        file = re.sub(r"^%s\/+src\/+" % rel, r'', file)
+        if not re.search(r'\/python\/', fname):
+          continue
+        with open(fname, 'r') as f:
+          for line in f.readlines():
+            if 'import ' in line:
+              line = line.rstrip('\n')
+              if re.search(r'^\s*#', line):
+                continue
+              match_from_import = re.search(r'^\s*from\s+([^\s]+)\s+import\s+', line)
+              match_import = re.search(r'^\s*import\s+([^\s]+)\s*', line)
+              if match_from_import:
+                for x in import2CMSSWDir(match_from_import.group(1), cache):
+                  if not cache.has_key("usedby"): cache["usedby"] = {}
+                  if not cache.has_key("uses"): cache["uses"] = {}
+                  if not cache["usedby"].has_key(x): cache["usedby"][x] = {}
+                  if not cache["uses"].has_key(file): cache["uses"][file] = {}
+                  if not cache["usedby"][x].has_key(file): cache["usedby"][x][file] = {}
+                  if not cache["uses"][file].has_key(x): cache["uses"][file][x] = {}
+                  cache["usedby"][x][file] = 1
+                  cache["uses"][file][x] = 1
+              elif match_import:
+                for x in import2CMSSWDir(match_import.group(1), cache):
+                  if not cache.has_key("usedby"): cache["usedby"] = {}
+                  if not cache.has_key("uses"): cache["uses"] = {}
+                  if not cache["usedby"].has_key(x): cache["usedby"][x] = {}
+                  if not cache["uses"].has_key(file): cache["uses"][file] = {}
+                  if not cache["usedby"][x].has_key(file): cache["usedby"][x][file] = {}
+                  if not cache["uses"][file].has_key(x): cache["uses"][file][x] = {}
+                  cache["usedby"][x][file] = 1
+                  cache["uses"][file][x] = 1
+  for type_ in ("uses","usedby"):
+    with open("%s/etc/dependencies/py%s.out" % (rel, type_), 'w') as ref:
+      for x in sorted(cache[type_].keys()):
+        ref.write("%s %s\n" % (x, " ".join(sorted(cache[type_][x].keys()))))
 
 def import2CMSSWDir(str, cache):
   pyfiles = []
+  if not cache.has_key("pymodule"): cache["pymodule"] = {}
+  if not cache.has_key("noncmsmodule"): cache["noncmsmodule"] = {}
   for s in str.split(","):
-    s = re.sub(r'\.', r'\/', s)
+    s = re.sub(r'\.', r'/', s)
     if s in cache["pymodule"]:
       pyfiles.append(cache["pymodule"][s])
     elif s not in cache["noncmsmodule"]:
@@ -121,29 +124,23 @@ def import2CMSSWDir(str, cache):
       else: cache["noncmsmodule"][s] = 1
   return pyfiles
 
-def buildFileDeps(rel, arch, scramroot):
-  pcache = {} # to store ProjectCache.db.gz dict
-  cache = {}
-  for dir in sorted(pcache["BUILDTREE"].keys()):
-    if pcache["BUILDTREE"][dir]["SUFFIX"] != "": continue
-    if len(pcache["BUILDTREE"][dir]["METABF"]) == 0: continue
-    bf = pcache["BUILDTREE"][dir]["METABF"][0]
-    bf = re.sub(r'src\/', r'',bf)
-    cache["dirs"][dir] = bf
-    pack = dir
-    class_ = pcache["BUILDTREE"][dir]["CLASS"]
-    if re.search( r'^(LIBRARY|CLASSLIB)$', class_):
-      pack = pcache["BUILDTREE"][dir]["PARENT"]
-    cache["packs"][pack] = dir
-  for dir in cache["dirs"].keys():
-    updateBFDeps(dir, pcache, cache)
-  for type_ in ("uses","usedby"):
-    with open("%s/etc/dependencies/bf%s.out" % (rel, type_), 'w') as ref:
-      for x in sorted(cache[type_].keys()):
-        ref.write("%s %s\n" % (x, " ".join(sorted(cache[type_][x].keys()))))
+def data2json(infile):
+  jstr = ""
+  rebs=re.compile(',\s+"BuildSystem::[a-zA-Z0-9]+"\s+\)')
+  revar=re.compile("^\s*\$[a-zA-Z0-9]+\s*=")
+  reundef=re.compile('\s*undef,')
+  lines = [l.strip().replace(" bless(","").replace("'",'"').replace('=>',' : ')  for l in
+  gzip.open(infile).readlines()]
+  lines[0] = revar.sub("",lines[0])
+  lines[-1] = lines[-1].replace(";","")
+  for l in lines:
+    l =  reundef.sub(' "",',rebs.sub("", l.rstrip()))
+    jstr += l
+  return json.loads(jstr)
 
 def updateBFDeps(dir, pcache, cache):
   bf = cache["dirs"][dir]
+  if not cache.has_key("uses"): cache["uses"] = {}
   if bf in cache["uses"]: return 0
   cache["uses"][bf] = {}
   for pack in pcache["BUILDTREE"][dir]["RAWDATA"]["DEPENDENCIES"].keys():
@@ -152,30 +149,51 @@ def updateBFDeps(dir, pcache, cache):
       updateBFDeps(xdata, pcache, cache)
       xdata = cache["dirs"][xdata]
       cache["uses"][bf][xdata] = 1
+      if not cache.has_key("usedby"): cache["usedby"] = {}
+      if not cache["usedby"].has_key(xdata): cache["usedby"][xdata] = {}
       cache["usedby"][xdata][bf] = 1
       for xdep in cache["uses"][xdata].keys():
         cache["uses"][bf][xdep] = 1
         cache["usedby"][xdep][bf] = 1
 
-import json, re, sys
-def data2json(infile):
-  jstr = ""
-  rebs=re.compile(',\s+"BuildSystem::[a-zA-Z0-9]+"\s+\)')
-  revar=re.compile("^\s*\$[a-zA-Z0-9]+\s*=")
-  reundef=re.compile('\s*undef,')
-  lines = [l.strip().replace(" bless(","").replace("'",'"').replace('=>',' : ')  for l in
-  open(infile).readlines()]
-  lines[0] = revar.sub("",lines[0])
-  lines[-1] = lines[-1].replace(";","")
-  for l in lines:
-    l =  reundef.sub(' "",',rebs.sub("", l.rstrip()))
-    jstr += l
-  return json.loads(jstr)
+def buildFileDeps(rel, arch, scramroot):
+  pcache = data2json("%s/.SCRAM/%s/ProjectCache.db.gz" % (rel, arch))
+  cache = {}
+  for dir in sorted(pcache["BUILDTREE"].keys()):
+    if pcache["BUILDTREE"][dir]["SUFFIX"] != "": continue
+    if len(pcache["BUILDTREE"][dir]["METABF"]) == 0: continue
+    bf = pcache["BUILDTREE"][dir]["METABF"][0]
+    bf = re.sub(r'src\/', r'',bf)
+    if not cache.has_key("dirs"): cache["dirs"] = {}
+    if not cache["dirs"].has_key(dir): cache["dirs"][dir] = {}
+    cache["dirs"][dir] = bf
+    pack = dir
+    class_ = pcache["BUILDTREE"][dir]["CLASS"]
+    if re.search( r'^(LIBRARY|CLASSLIB)$', class_):
+      pack = pcache["BUILDTREE"][dir]["PARENT"]
+      if not cache.has_key("packs"): cache["packs"] = {}
+      if not cache["packs"].has_key(pack): cache["packs"][pack] = {}
+    cache["packs"][pack] = dir
+  for dir in cache["dirs"].keys():
+    updateBFDeps(dir, pcache, cache)
+  for type_ in ("uses","usedby"):
+    with open("%s/etc/dependencies/bf%s.out" % (rel, type_), 'w') as ref:
+      if not cache.has_key("type_"): cache["type_"] = {}
+      for x in sorted(cache[type_].keys()):
+        ref.write("%s %s\n" % (x, " ".join(sorted(cache[type_][x].keys()))))
 
 for root, dirs, files in os.walk(directory):
   for filename in files:
     name = os.path.join(root, filename)
-    if re.search( r'^.*(\.dep|\/a\/xr+\.cc\.d)\z', name): doexec(0, 'cat','{}')
+    if re.search( r'^.*(\.dep|\/a\/xr+\.cc\.d)', name): doexec()
+
+with open(rel + "/etc/dependencies/uses.out", 'w') as file:
+  for key, value in sorted(uses.items()):
+    file.write("%s %s\n" % (key,value))
+
+with open(rel + "/etc/dependencies/usedby.out", 'w') as file:
+  for key, value in sorted(usedby.items()):
+    file.write("%s %s\n" % (key,value))
 
 pythonDeps(rel)
 buildFileDeps(rel, scramarch, scramroot)
